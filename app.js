3096f37c422fff3b931d50763a04aa6e0381881f
Kyle Johnson <joh04667@gmail.com>
2016-03-29 13:03:36 -0500

    Adds many, many more randomized names and jobs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var employee = {};
+var totalSalary = 0;
 
-var firstNameArr = ["Mark", "Peter", "Squanto", "Suzanne", "Chris", "Kyle", "Jen", "Joel", "Ryan", "Millie", "Travis", "Lisa", "Caroline", "Ed", "Oliver"];
-var lastNameArr = ["Squeaky", "Potts", "Potter", "Granger", "Bush", "Johnson", "Obama", "Clinton", "Trump", "Edwards", "Gawarecki", "Hanson", "Sanders"];
-var titleArr = ["Floor Clearner", "CEO", "CTO", "Babysitter", "Nuclear Materials Handler", "Kitten Whisperer", "Executive Producer", "Envelope Licker", "Undercover Bum", "Alien Conspiracy Writer", "Executive in Charge of Imaginary Studies", "Boot Shiner", "Cowboy Wrangler"];
+var firstNameArr = ["Mark", "Peter", "Squanto", "Suzanne", "Chris", "Kyle", "Jen", "Joel", "Ryan", "Millie", "Travis", "Lisa", "Caroline", "Ed", "Oliver", "Paul", "George", "Ringo", "Genghis", "Albus", "Charlie", "Dennis", "Dee", "Ronald", "Frank", "Carmen", "Chandler", "Joey", "Regina"];
+var lastNameArr = ["Squeaky", "Potts", "Potter", "Granger", "Bush", "Johnson", "Obama", "Clinton", "Trump", "Edwards", "Gawarecki", "Hanson", "Sanders", "Khan", "Star", "Dumbledore", "Day", "Reynolds", "McDonald", "Sandiego", "Bing", "Tribbiani", "Phalange"];
+var titleArr = ["Floor Clearner", "CEO", "CTO", "Babysitter", "Nuclear Materials Handler", "Kitten Whisperer", "Executive Producer", "Envelope Licker", "Undercover Bum", "Alien Conspiracy Writer", "Executive in Charge of Imaginary Studies", "Boot Shiner", "Cowboy Wrangler", "Meme Creator", "Superpower Researcher", "Dust Collector", "Office Clown", "Paper Airplane Maker", "Bathroom Inspector", "Transponster", "Door Opener", "the guy who orders lunch", "Accountant (but actually just on reddit all day)", "Brownnoser", "Karaoke Guru", "Mic Dropper", "Ice Cube Weigher", "Food Heater Upper", "Cube Stacker", "person who turns on the television when we can't find the remote", "Minister of Magic", "Roller Coaster Tester", "Electric Socket Tester", "King of the Rats", "Children's Cereal Mascot", "Nobody really knows what they do but we give them money", "Human Ladder", "Cheerleader", "Backseat Driver", "Dives for Pennies in Mall Fountains", "Doesn't know the shortcuts for Copy and Paste so types everything out", "Searches for google.com to get to Google", "Firebender", "Robot", "The Chosen One", "Interplanetary Ambassador", "Facial Hair Rater"];
 
 $(function() {
   $("#employee").on("submit", function(event) {
@@ -20,6 +21,8 @@ $(function() {
     firstNameArr.push(this.first_name.value);
     lastNameArr.push(this.last_name.value);
     titleArr.push(this.job_title.value);
+
+
 
     // Clear inputs
     $("#first_name").val("");
@@ -43,7 +46,8 @@ $(function() {
     employee.employeeNumber = Math.floor((Math.random() * 90999) + 10000);
     employee.lastReviewScore = Math.floor(Math.random() * 5)+1;
     employee.salary = Math.floor((Math.random() * 200000) + 25000);
-    
+
+
     displayEmployee();
   });
 });
@@ -58,8 +62,10 @@ function displayEmployee() {
   var score = scoreLi + "<span>Last Review Score:</span> " + employee.lastReviewScore + "</li>";
 
   var salary = "<li><span>Salary:</span> " + formatAsCurrency(parseInt(employee.salary)) + "</li>";
-  console.log("working");
-  $('#display').append( name+ "<ul>" + number + title + score + salary + "</ul></div>");
+  totalSalary += employee.salary;
+  console.log(totalSalary);
+
+  $('#display').prepend( name+ "<ul>" + number + title + score + salary + "</ul></div>");
 }
 
 // Format input as currency
